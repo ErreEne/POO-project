6805c252ada8496e4330a5652709bbeebed0dec7
André Teodósio <andre.teodosio@tecnico.ulisboa.pt>
2023-05-23 22:35:44 +0100

    Main, não se diz asneiras

---
+++ GrafoPack/Main.java
@@ -0,0 +1,12 @@
+package GrafoPack;
+
+public class Main {
+    public static void main(String[] args) {
+        Grafo grafo = new Grafo(5,5,8);
+        grafo.MostrarVerticeInfo(1);
+        grafo.MostrarVerticeInfo(2);
+        grafo.MostrarVerticeInfo(3);
+        grafo.MostrarVerticeInfo(4);
+        grafo.MostrarVerticeInfo(5);
+    }
+}
