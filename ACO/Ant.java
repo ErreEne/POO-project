43e8ae26d8f1216fd637919280ebad2f828b7d90
André Teodósio <andre.teodosio@tecnico.ulisboa.pt>
2023-05-26 14:23:37 +0100

    Changes by Tomás Martins

--- ACO/Ant.java
+++ ACO/Ant.java
@@ -5,26 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ant {
-    Grafo grafo;
-    public int distance;
-    public int[] visited_nodes;
-    public int[] unvisited_nodes;
-    public int[] path;
-    public int beta, alpha, gamma, delta, eta;
-    public Ant(Grafo grafo, int nest_node, int beta, int alpha, int gamma, int delta, int eta) {
-        this.grafo = grafo;
-        this.visited_nodes = new int[grafo.getN()];
-        this.unvisited_nodes = new int[grafo.getN()];
-        this.path = new int[grafo.getN()];
-        this.distance = 0;
-        this.visited_nodes[0] = nest_node;
-        this.unvisited_nodes[nest_node] = 0;
-        this.path[0] = nest_node;
-        this.beta = beta;
-        this.alpha = alpha;
-        this.gamma = gamma;
-        this.delta = delta;
-        this.eta = eta;
+    public ArrayList<Integer> path;
+    public ArrayList<Integer> visitedNodes;
+    public ArrayList<Integer> unVisitedNodes;
+
+
+    public Ant() {
+        super();
+        this.path = new ArrayList<Integer>();
+        this.visitedNodes = new ArrayList<Integer>;
+        this.unVisitedNodes = new ArrayList<Integer>();
     }
 
     public static double getProbability(double alfa, double beta, int no, double[] weight, double[] pheromone, int nNodes){
@@ -43,6 +33,27 @@ public class Ant {
         } else {
             removeLoop(newNode);
         }
+    }
+
+    public void removeLoop(int nodeToRemove) {
+        this.path.remove(nodeToRemove - 1);
+    }
+
+    public void addToPath(int nodeToAdd) {
+        this.path.add(nodeToAdd);
+    }
+
+    public int checkSize(ArrayList<Integer> listToCheck) {
+        return listToCheck.size();
+    }
+
+    public int checkLoop(int newNode) {
+        for (int i = 0; i < this.checkSize(this.path); i++) {
+            if(this.path.get(i) == newNode) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public double pheromoneLevel (int gama) {
