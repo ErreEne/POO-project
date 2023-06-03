740ac84ab0a6fec8f25f4370a89918c12f431f54
André Teodósio <andre.teodosio@tecnico.ulisboa.pt>
2023-06-03 09:44:05 +0100

    ?

--- DSS/AntMove.java
+++ DSS/AntMove.java
@@ -3,5 +3,6 @@ package DSS;
 public class AntMove implements Event {
 
     public void execute() {
+        // Chamar coisas na Ant que façam-na mover para o próximo nó e depois gerar o timestamp do próximo evento
     };
 }
--- DSS/EvaporationOfPheromone.java
+++ DSS/EvaporationOfPheromone.java
@@ -3,9 +3,7 @@ package DSS;
 public class EvaporationOfPheromone implements Event{
 
     public void execute(){
-
-
-
+        // Chamar coisas em Miguel?? que evaporem e depois gerar o timestamp do próximo evento
     };
 
 }
