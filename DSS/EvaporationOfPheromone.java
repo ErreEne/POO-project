c6e5d734bf4c440457b52d566986a3c8edfcb6e5
André Teodósio <andre.teodosio@tecnico.ulisboa.pt>
2023-06-04 09:21:19 +0100

    .

--- DSS/EvaporationOfPheromone.java
+++ DSS/EvaporationOfPheromone.java
@@ -1,5 +1,6 @@
 package DSS;
 
+import ACO.Miguel;
 import ACO.MiguelInter;
 import java.util.ArrayList;
 
@@ -8,6 +9,7 @@ public class EvaporationOfPheromone implements FeromonasInterface {
     public void execute() {
         // Chamar coisas em Miguel?? que evaporem e depois gerar o timestamp do próximo
         // evento
+        Miguel.evaporationOfPheromone(); // precisa de ser static?
     };
 
     public void GenerateQueue(ArrayList<MiguelInter> array) {
